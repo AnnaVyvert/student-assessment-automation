9bc77755d7cb99b8c5b4b3441f3900a4ceac243e
anvy <0073av@gmail.com>
2022-11-20 01:29:14 +0800

    list page connected

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import NotFoundPage from "./page/404page";
+import ListPage from "./page/list_page";
 import SignPage from "./page/sign";
 // import MenuBar from './components/sideMenu/sideMenu';
 
@@ -12,7 +13,10 @@ const App = () => {
         <>
           {/* <MenuBar /> */}
           <Routes>
-            <Route path="/sign" element={<SignPage />} />
+            {/* <Route path="/sign" element={<SignPage />} /> */}
+            <Route path="/group-list" element={<ListPage role_id={1}/>} />
+            <Route path="/student-list" element={<ListPage role_id={2}/>} />
+            <Route path="/subject-list" element={<ListPage role_id={3}/>} />
            
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
---
+++ src/page/list_page.jsx
@@ -0,0 +1,161 @@
+import React, { useState } from 'react';
+import { del_req, get_req } from '../server-api/requests_api';
+import Popup from '../popup/popup_base';
+import MenuBar from '../components/side_menu/side_menu';
+import { accountValidate } from '../utills/account_validate';
+import NotFoundPage from './404page';
+import SearchBar from '../components/search_bar/search_bar';
+  
+const ListPage = ({ role_id }) => {
+  const [id, set_id] = useState(1);
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+  const [currentPopup, setCurrentPopup] = useState('');
+  const requests = get_req_router(role_id);
+  const elem = get_req(requests.get);
+  const [searchResults, setSearchResults] = useState(elem);
+  // console.log(searchResults);
+  document.title = 'Список';
+  const acc_json = { id: id, role_id: role_id, get_info: requests.get_info };
+  // const search = document.querySelector('.search-bar') || {}
+  // search.value=''
+
+  const active_popup = (a) => {
+    setCurrentPopup(a);
+    setIsPopupVisible(true);
+  };
+  // const elem = get_req(`/api/account/${role_id}`)
+
+  return (
+    <>
+      {isPopupVisible && (
+        <Popup
+          active={isPopupVisible}
+          setActive={setIsPopupVisible}
+          nameWindow={currentPopup}
+          param={{ entity: requests.list, post_api: '/api/' + requests.list }}
+          bool={true}
+          obj={acc_json}
+        />
+      )}
+      <span onClick={()=>setSearchResults(elem)}>
+      {/* на эту строчку потрачено 4 часа, она нужна для обновления списка после поиска и перехода в другой раздел меню */}
+        <MenuBar />
+      </span>
+      <div
+        style={{
+          backgroundColor: '#202020',
+          textAlign: 'center',
+          // borderRadius: '0.5em',
+          fontVariantCaps: 'all-petite-caps',
+          fontSize: '1.4em',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: '2em',
+            paddingTop: '1.4em',
+            fontWeight: 'bold',
+          }}
+        >
+          {requests.title}
+        </div>
+        <div>
+          <SearchBar
+            elem={elem}
+            setSearchResults={setSearchResults}
+            type={requests.list}
+          />
+        </div>
+      </div>
+      <div
+        style={{  }}
+      >
+        <div className="wrap">
+          <table className='table-editable'>
+            <thead>
+              <tr>
+                <td>Шифр</td>
+                <td>Год</td>
+                <td>Номер</td>
+                <td></td>
+              </tr>
+            </thead>
+            <tbody>
+            {searchResults.map((elem, i) => (
+                <tr
+                  className="competition_list_row"
+                  key={i}
+                  onClick={() => {
+                    // navigate('/competition?id=' + elem.id);
+                  }}
+                >
+                  <td>
+                    {elem[requests.fields[0]]}
+                  </td>
+                  <td>{elem[requests.fields[1]]}</td>
+                  <td>{elem[requests.fields[2]]}</td>
+                  <td>
+                    удалить
+                  </td>
+                </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {(
+        <button
+          className="absolute-bottom-btn"
+          onClick={() => {
+            active_popup('reg_com');
+          }}
+        >
+          Добавить
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ListPage;
+
+function get_req_router(role_id) {
+  switch (role_id) {
+    case 1:
+      return {
+        list: 'group',
+        get: 'groups',
+        info: 'account_info',
+        create: 'reg_ath',
+        delete: 'group/',
+        title: 'Список групп',
+        get_info: 'group/',
+        fields: ['cipher', 'start_year', 'number'],
+      };
+    case 2:
+      return {
+        list: 'student',
+        get: 'students',
+        info: 'account_info',
+        create: 'reg_trainer',
+        delete: 'student/',
+        title: 'Список  студентов',
+        get_info: 'student/',
+        fields: ['surname', 'name', 'patronym'],
+      };
+    case 3:
+      return {
+        list: 'subject',
+        get: 'subjects',
+        info: 'account_info',
+        create: 'null',
+        delete: 'subject/',
+        title: 'Список предметов',
+        get_info: 'subject/',
+        fields: ['name', 'hours', 'exam'],
+      };
+    default:
+      return [];
+  }
+}
