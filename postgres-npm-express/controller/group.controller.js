86d3416efaf4835401bbfaee43ec124787e3c123
anvy <0073av@gmail.com>
2022-11-19 11:13:55 +0800

    complete group

--- postgres-npm-express/controller/group.controller.js
+++ postgres-npm-express/controller/group.controller.js
@@ -4,13 +4,13 @@ class group_controller {
     async create(req, res){
         const {number, start_year, cipher} = req.body
         try{
-            const call = await db.query(`INSERT INTO public.group (number, start_year, cipher) values ($1, $2, $3) RETURNING *`,
-            [number, start_year, cipher])
+            const call = await db.query(`INSERT INTO public.group (number, start_year, cipher, deleted) values ($1, $2, $3, $4) RETURNING *`,
+            [number, start_year, cipher, false])
             res.json(call.rows[0])
         }catch(e){res.status(400).send(e)}
     }
     async get_all(req, res){
-        const call = await db.query(`SELECT * FROM public.group`)
+        const call = await db.query(`SELECT id, number, start_year, cipher FROM public.group where deleted = false`)
         res.json(call.rows)
     }
     async get_one(req, res){
@@ -33,8 +33,6 @@ class group_controller {
     }
     async del_one(req, res){
         const id = req.params.id
-        // const dependences = await db.query(`DELETE FROM competition2athlet where athlet_id = ${id}`)
-        // const call = await db.query(`DELETE FROM athlet where id = ${id}`)
         const call = await db.query(`UPDATE public.group set deleted = true where id = ${id} RETURNING *`)
         res.json(call.rows[0])
     }
