bf21326072b271114ba058a731138c4ba81447fe
anvy <0073av@gmail.com>
2022-11-19 19:54:35 +0800

    side menu

---
+++ src/components/side_menu/side_menu.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { accountValidate } from "../../utills/account_validate";
+import { getRights } from "../../utills/get_rights";
+import './side_menu.css'
+
+const MenuBar = () => {
+  const navigate = useNavigate()
+  const sideNav = useRef()
+  const sideToNav = useRef()
+  const menu_point = (value, link) => <span onClick={()=>navigate(`${link}`)}>{value}</span>
+  const menu = 
+  <>
+    <div ref={sideNav} className="sidenav" onClick={()=>{}}>
+      <div style={{ height: '3em'}} />
+      {/* {menu_point('Личный кабинет', '/personal')} */}
+      {/* {menu_point('Авторизация', '/sign')} */}
+      {menu_point('Список групп', '/group-list')}
+      {menu_point('Список студентов', '/student-list')}
+      {menu_point('Список предметов', '/subject-list')}
+      {menu_point('Посмотреть промежуточную ведомость', '/edit-mark-form')}
+    </div>
+    <div ref={sideToNav} className="side-to-nav" onClick={()=>{openMenu(false)}} />
+  </>
+
+  function openMenu(bool) {
+    if(bool){
+      sideNav.current.style.width = "480px"
+      sideToNav.current.style.width = "100%"
+    }else{
+      sideNav.current.style.width = "0"
+      sideToNav.current.style.width = "0"
+    }
+    // document.getElementById("mySidenav").style.width = "230px";
+  }
+    
+  return(
+    <>
+        {menu}
+        <span className="menu-btn" onClick={()=>{openMenu(true)}}>&#9776;</span>
+    </>
+  )
+}
+
+export default MenuBar
