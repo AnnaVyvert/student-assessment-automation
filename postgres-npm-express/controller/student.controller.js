0d9cef78230f6ac250bfc3d87cae6fa778be3dd1
anvy <0073av@gmail.com>
2022-11-19 11:28:41 +0800

    complete student

--- postgres-npm-express/controller/student.controller.js
+++ postgres-npm-express/controller/student.controller.js
@@ -2,24 +2,13 @@ const db = require('../database.js')
 
 class student_controller {
     async create(req, res){
-        const {name, distance, start} = req.body
-        const creator_id = req.params.creator_id
-        const competition_id = req.params.competition_id
+        const {name, surname, patronym, sex, birth, group_id} = req.body
         try{
-
-            const age_translate = (age) =>{
-                switch(age){
-                    case 'С 12 до 16 лет': return 0
-                    case 'С 16 до 18 лет': return 1
-                }
-            }
-            const age_id = age_translate(req.body.age_id)
-            const sex = req.body.sex === 'Ж'
-            console.log(name, sex, distance, age_id, start, false, creator_id, competition_id)
-            const call = await db.query(`INSERT INTO public.student (name, sex, distance, age_id, start, deleted, creator_id, competition_id) values ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
-            [name, sex, distance, age_id, start, false, creator_id, competition_id])
+            const call = await db.query(`INSERT INTO public.student (name, surname, patronym, sex, birth, group_id, deleted) 
+            values ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+            [name, surname, patronym, sex, birth, group_id, false])
             res.json(call.rows[0])
-        }catch(e){res.status(400).send(e)}//res.json('400 Bad Request')
+        }catch(e){res.status(400).send(e)}
     }
     async get_one(req, res){
         const id = req.params.id
@@ -29,7 +18,7 @@ class student_controller {
         }catch(e){res.status(400).send(e)}
     }
     async get_all(req, res){
-        const call = await db.query(`SELECT * FROM public.student`)
+        const call = await db.query(`SELECT id, name, surname, patronym, sex, birth, group_id FROM public.student where deleted = false`)
         res.json(call.rows)
     }
     async update_parameter(req, res){
@@ -45,8 +34,6 @@ class student_controller {
     }  
     async del_one(req, res){
         const id = req.params.id
-        const dependences = await db.query(`UPDATE athlet set trainer_id = null where trainer_id = ${id}`)
-        
         const call = await db.query(`UPDATE public.student set deleted = true where id = ${id} RETURNING *`)
         res.json(call.rows[0])
     }
