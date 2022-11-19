1e0d798d31761a04ac41fdd555a42f0bcb7cf896
anvy <0073av@gmail.com>
2022-11-19 11:33:23 +0800

    complete subject

--- postgres-npm-express/controller/subject.controller.js
+++ postgres-npm-express/controller/subject.controller.js
@@ -2,15 +2,16 @@ const db = require('../database.js')
 
 class subject_controller {
     async create(req, res){
-        const {name, surname, patronym, phone} = req.body
+        const {name, hours, exam} = req.body
         try{
-            const call = await db.query(`INSERT INTO public.subject (name, surname, patronym, phone, deleted) values ($1, $2, $3, $4, false) RETURNING *`,
-            [name, surname, patronym, phone])
+            const call = await db.query(`INSERT INTO public.subject (name, hours, exam, deleted) 
+            values ($1, $2, $3, $4) RETURNING *`,
+            [name, hours, exam, false])
             res.json(call.rows[0])
-        }catch(e){res.status(400).send(e)}//res.json('400 Bad Request')
+        }catch(e){res.status(400).send(e)}
     }
     async get_all(req, res){
-        const call = await db.query(`SELECT * FROM public.subject`)
+        const call = await db.query(`SELECT id, name, hours, exam FROM public.subject where deleted = false`)
         res.json(call.rows)
     }
     async get_one(req, res){
@@ -33,8 +34,6 @@ class subject_controller {
     }
     async del_one(req, res){
         const id = req.params.id
-        const dependences = await db.query(`UPDATE athlet set public.subject_id = null where public.subject_id = ${id}`)
-        
         const call = await db.query(`UPDATE public.subject set deleted = true where id = ${id} RETURNING *`)
         res.json(call.rows[0])
     }
