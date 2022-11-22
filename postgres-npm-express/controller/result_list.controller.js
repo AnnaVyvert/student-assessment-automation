cc2ad5cd4d5f5905ab6509604ab0d0f4ccd7bad3
anvy <0073av@gmail.com>
2022-11-23 00:53:04 +0800

    post fix and add new req

--- postgres-npm-express/controller/result_list.controller.js
+++ postgres-npm-express/controller/result_list.controller.js
@@ -2,10 +2,11 @@ const db = require('../database.js');
 
 class result_list_controller {
   async create(req, res){
-    const {} = req.body
+    const {student_id, subject_id, mark, mark_date} = req.body
+    console.log(student_id, subject_id, mark, mark_date)
     try{
-        const call = await db.query(`INSERT INTO public.group (, deleted) values ($1, $2, $3, $4) RETURNING *`,
-        [, false])
+        const call = await db.query(`INSERT INTO public.result_list (student_id, subject_id, mark, date, deleted) values ($1, $2, $3, $4, $5) RETURNING *`,
+        [student_id, subject_id, mark, mark_date, false])
         res.json(call.rows[0])
     }catch(e){res.status(400).send(e)}
   }
@@ -19,6 +20,21 @@ class result_list_controller {
   async get_all(req, res) {
     const call = await db.query(`SELECT id, student_id, subject_id, mark, date FROM public.result_list where deleted = false`)
     res.json(call.rows)
+  }
+  async get_final_list(req, res){
+    // const call = await db.query(`select *, AVG(mark::int) from public.result_list;`)
+    const call = await db.query(`select AVG(mark::int) from public.result_list where student_id = 1 and subject_id = 1`)
+  }
+  async get_marks(req, res){
+    const subject_id = req.params.subject_id
+    const group_id = req.params.group_id
+    // const max_n_mark = await db.query(`select max(count) from (select count(*) from public.result_list where deleted = false group by subject_id) as mark_n`)
+    const call = await db.query(`select *, to_char("date", 'DD/MM/YYYY') as mark_date from public.result_list rl, public.student st 
+    where rl.student_id = st.id and rl.deleted = false and rl.subject_id = $1 and st.group_id = $2 
+    order by rl.date NULLS LAST`,
+    [subject_id, group_id])
+    res.json(call.rows)
+
   }
   async update_parameter(req, res){
     const id = req.params.id
