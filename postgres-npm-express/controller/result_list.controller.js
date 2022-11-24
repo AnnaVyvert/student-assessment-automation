3b75a03f495df4917399e01ae2e8d7653f84efbe
anvy <0073av@gmail.com>
2022-11-24 12:06:14 +0800

    new queries and routers result_list

--- postgres-npm-express/controller/result_list.controller.js
+++ postgres-npm-express/controller/result_list.controller.js
@@ -23,18 +23,35 @@ class result_list_controller {
   }
   async get_final_list(req, res){
     // const call = await db.query(`select *, AVG(mark::int) from public.result_list;`)
-    const call = await db.query(`select AVG(mark::int) from public.result_list where student_id = 1 and subject_id = 1`)
+    const call = await db.query(`select student_id, subject_id, su_name, st_id, st_surname, st_name, st_patronym, avg(mark::int) from 
+    (select a.*, b.id as su_id, b.name as su_name from 
+    (SELECT a.*, b.id as st_id, b.name as st_name, b.surname as st_surname, b.patronym as st_patronym 
+    from 
+    result_list a
+    INNER JOIN student b ON b.id = a.student_id where b.deleted = false and a.deleted = false) 
+    a INNER JOIN subject b ON b.id = a.subject_id) as two 
+    group by student_id, subject_id, su_name, st_id, st_surname, st_name, st_patronym order by st_surname;
+    `)
+    res.json(call.rows)
   }
   async get_marks(req, res){
     const subject_id = req.params.subject_id
     const group_id = req.params.group_id
-    // const max_n_mark = await db.query(`select max(count) from (select count(*) from public.result_list where deleted = false group by subject_id) as mark_n`)
+    const call = await db.query(`select max(count) from (select count(*) from public.result_list where deleted = false group by student_id, subject_id) as mark_n;`)
+    // const call = await db.query(`select *, to_char("date", 'DD/MM/YYYY') as mark_date from public.result_list rl, public.student st 
+    // where rl.student_id = st.id and rl.deleted = false and rl.subject_id = $1 and st.group_id = $2 
+    // order by rl.date NULLS LAST`,
+    // [subject_id, group_id])
+    res.json(call.rows[0].max)
+  }
+  async get_marks_by_group_subject(req, res){
+    const subject_id = req.params.subject_id
+    const group_id = req.params.group_id
     const call = await db.query(`select *, to_char("date", 'DD/MM/YYYY') as mark_date from public.result_list rl, public.student st 
     where rl.student_id = st.id and rl.deleted = false and rl.subject_id = $1 and st.group_id = $2 
     order by rl.date NULLS LAST`,
     [subject_id, group_id])
     res.json(call.rows)
-
   }
   async update_parameter(req, res){
     const id = req.params.id
--- postgres-npm-express/routes/result_list.routes.js
+++ postgres-npm-express/routes/result_list.routes.js
@@ -4,7 +4,11 @@ const result_list_controller = require('../controller/result_list.controller')
 
 router.post('/result_list', result_list_controller.create)
 router.get('/result_list', result_list_controller.get_all)
-router.get('/result_list/:id', result_list_controller.get_one)
+router.get('/result_list/id/:id', result_list_controller.get_one)
+router.get('/result_list/final', result_list_controller.get_final_list)
+
+router.get('/result_list/max_marks', result_list_controller.get_marks)
+router.get('/result_list/marks/:subject_id/:group_id', result_list_controller.get_marks_by_group_subject)
 router.put('/result_list/:id/:column/:parameter', result_list_controller.update_parameter)
 router.delete('/result_list/:id', result_list_controller.del_one)
 
